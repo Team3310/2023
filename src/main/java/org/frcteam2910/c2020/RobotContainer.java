5bd11189c48d7af06f5bcf5b7f9e04c1c9c4da20
Drew Mitchell <drew.mitchell8398@gmail.com>
2023-03-17 22:06:03 -0500

    Quals 52 - Remove cube safety code since it locked up the arm, clean up ScoreMode slightly

--- src/main/java/org/frcteam2910/c2020/RobotContainer.java
+++ src/main/java/org/frcteam2910/c2020/RobotContainer.java
@@ -11,11 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 
 public class RobotContainer {
@@ -159,9 +155,9 @@ public class RobotContainer {
         secondaryController.getRightTriggerAxis().onTrue(
             new SequentialCommandGroup(
                 new SetIntakeRPM(Intake.getInstance(), Constants.INTAKE_COLLECT_RPM),
-                new SetArmSafely(ScoreMode.CUBE_INTAKE),
-                new WaitUntilCommand(() -> intake.getCubeSensor().get()),
-                new InstantCommand(() -> intake.setIntakeHold())
+                new SetArmSafely(ScoreMode.CUBE_INTAKE)
+                // For now, it's up to the operator to stop intaking to not pop the cube
+                
                 // Hasn't been tested, but an idea: need to stop intaking when intake.getCubeSensor().get() is true
                 // new SequentialCommandGroup(
                 //     new ParallelRaceGroup(
--- src/main/java/org/frcteam2910/c2020/util/ScoreMode.java
+++ src/main/java/org/frcteam2910/c2020/util/ScoreMode.java
@@ -5,7 +5,7 @@ public enum ScoreMode{
     CONE_INTAKE(15, 0.1), //17, 3.375 practice : 15 , 0.1
     
     MID(85, 0.875),
-    HIGH(103.4, 13.5), // "money" - paul
+    HIGH(103, 13.5), // "money" - paul
     LOW(40, 0.1),
     CUBE_INTAKE(24.5, 5.25); //33, 9.5 practice : 24.5, 4.25
 
@@ -44,31 +44,14 @@ public enum ScoreMode{
     }
 
     public double getAutonAngle(){
-        if(this == CUBE_INTAKE || this == CONE_INTAKE){
-            return angle;
-        }
-        else{
-            if(this == CUBE_INTAKE || this == CONE_INTAKE){
-                return angle;
-            }
-            else{
-                if(false){
-                    switch(this){
-                        case HIGH : return -100;
-                        case MID : return -80.0;
-                        case LOW : return -40;
-                        default : return this.getAngle();
-                    }
-                }
-                else{
-                    switch(this){
-                        case HIGH : return -102.4;
-                        case MID : return -85;
-                        case LOW : return -40;
-                        default : return this.getAngle();
-                    }
-                }
-            }
+        switch(this){
+            case CUBE_INTAKE:
+            case CONE_INTAKE:
+                return this.getAngle();
+            case HIGH : return -102.4;
+            case MID : return -85;
+            case LOW : return -40;
+            default : return this.getAngle();
         }
     }
 
